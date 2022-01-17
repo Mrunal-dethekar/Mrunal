72b821ace946a7443fdb27d2c29345f8e6235879
Mrunal-dethekar <95629099+Mrunal-dethekar@users.noreply.github.com>
2022-01-17 19:07:48 +0530

    ok

---
+++ Interactive Search/script.js
@@ -0,0 +1,8 @@
+const searchxyz = document.querySelector(".search")
+const inputxyz  = document.querySelector(".input")
+const btnxyz    = document.querySelector(".btn")
+
+btnxyz.addEventListener("click",function(){
+    searchxyz.classList.toggle("active")
+    inputxyz.focus()
+})
