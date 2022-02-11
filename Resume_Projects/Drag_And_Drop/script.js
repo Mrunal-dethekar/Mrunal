f0cadb1bd70288d0930824ed08382e86f3ad311d
Mrunal-dethekar <95629099+Mrunal-dethekar@users.noreply.github.com>
2022-02-11 17:47:01 +0530

    changes

--- Resume_Projects/Drag_And_Drop/script.js
+++ Resume_Projects/Drag_And_Drop/script.js
@@ -1,43 +1,74 @@
-const listItems = document.querySelectorAll(".list-item")
-const lists = document.querySelectorAll(".list")
+const submitBtn = document.getElementById("submitbtn")
+const addTaskList =document.getElementById("addTaskList")
+const inputTask = document.getElementById("addTask")
 
-let draggedItem
 
-listItems.forEach((item)=>{
-  item.addEventListener("dragstart", ()=>{
-    draggedItem=item
-    setTimeout(()=>{
-      item.style.display= 'none'
-    },0)
-  })
-
-  item.addEventListener("dragend", ()=>{
-    setTimeout(()=>{
-      draggedItem.style.display = 'block'
-      draggedItem=null
+function updateElements(){
+  const listItems = document.querySelectorAll(".list-item")
+  const lists = document.querySelectorAll(".list")
+  
+  let draggedItem=''
+  
+  listItems.forEach((item)=>{
+    item.addEventListener("dragstart", ()=>{
+      draggedItem=item
+      setTimeout(()=>{
+        item.style.display= 'none'
+      },0)
     })
-  })
-
-  lists.forEach((list)=>{
-
-    list.addEventListener("dragover", (e)=>{
+  
+    item.addEventListener("dragend", ()=>{
+      setTimeout(()=>{
+        draggedItem.style.display = 'block'
+        draggedItem=''
+      },0)
+    })
+  
+    item.addEventListener("contextmenu",(e)=>{
       e.preventDefault()
+      item.remove()
     })
-
-    list.addEventListener("dragenter", function (e){
-      e.preventDefault()
-      this.style.backgroundColor = 'rgba(0, 0, 0, 0.2)'
+  
+    lists.forEach((list)=>{
+  
+      list.addEventListener("dragover", (e)=>{
+        e.preventDefault()
+      })
+  
+      list.addEventListener("dragenter", function (e){
+        e.preventDefault()
+        this.style.backgroundColor = 'rgba(0, 0, 0, 0.2)'
+      })
+  
+      list.addEventListener("dragleave", function(){
+        this.style.backgroundColor = 'rgba(0, 0, 0, 0.1)'
+      })
+  
+      list.addEventListener("drop", function (){
+        list.append(draggedItem)
+        this.style.backgroundColor = 'rgba(0, 0, 0, 0.1)'
+      })
+  
     })
-
-    list.addEventListener("dragleave", function(){
-      this.style.backgroundColor = 'rgba(0, 0, 0, 0.1)'
-    })
-
-    list.addEventListener("drop", function (){
-      list.append(draggedItem)
-      this.style.backgroundColor = 'rgba(0, 0, 0, 0.1)'
-    })
-
+  
   })
+  
+}
 
+updateElements()
+
+submitBtn.addEventListener("click",(e)=>{
+  e.preventDefault()
+  const task = inputTask.value
+  inputTask.value = ''
+  if(task){
+    const newDiv = document.createElement('div')
+    newDiv.classList.add("list-item")
+    newDiv.setAttribute("draggable","true")
+    newDiv.innerText=task
+    addTaskList.appendChild(newDiv)
+    updateElements()
+  }else{
+    alert("Please Type A Task")
+  }
 })
