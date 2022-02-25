e4c8f35a56184ddbe48a6ed91c4de0dcbcf8f6f9
Mrunal-dethekar <95629099+Mrunal-dethekar@users.noreply.github.com>
2022-02-25 18:44:01 +0530

    25thFeb

--- React/react-intro-2/src/App.js
+++ React/react-intro-2/src/App.js
@@ -1,16 +1,18 @@
 import "./App.css";
-import Hello from "./component/HelloWorld";
+// import Hello from "./component/HelloWorld";
 import HelloClass from "./component/HelloClass";
+import  React, { Component } from "react";
 
-function App() {
+class App extends Component{
+  render(){
   return (
     <>
-      <Hello heading="India" heading2="MH" />
+      {/* <Hello heading="India" heading2="MH" />
       <Hello heading="Africa" />
-      <Hello heading="USA" />
-      <HelloClass/>
+      <Hello heading="USA" /> */}
+      <HelloClass />
     </>
   );
 }
-
+}
 export default App;
--- React/react-intro-2/src/component/HelloClass.js
+++ React/react-intro-2/src/component/HelloClass.js
@@ -1,9 +1,40 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+
+// class HelloClass extends Component {  
+// or we can write React.component and delete it from import
+
+//   render() {
+//     return (
+//       <h1>Hello class {this.props.heading}</h1>
+//     );
+//   }
+// }
 
 class HelloClass extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      greetTo: "World",
+      greetAlsoTo: "Australia",
+    };
+  }
+
+  changeGreetTo = () => {
+    this.setState({
+      greetTo: "India",
+    });
+  };
+
   render() {
+    const { greetTo, greetAlsoTo } = this.state;
     return (
-      <h1>Hello class</h1>
+      <>
+        <h1>
+          Hello class {greetTo} and {greetAlsoTo}
+          <button onClick={this.changeGreetTo}>Change State</button>
+        </h1>
+      </>
     );
   }
 }
