d4a62c950824692e0167a34c0f72556a946f5908
Mrunal-dethekar <95629099+Mrunal-dethekar@users.noreply.github.com>
2022-03-15 20:02:23 +0530

    15thMar

---
+++ React/react-5/redux.js
@@ -0,0 +1,14 @@
+const redux = require('redux')
+
+// Action Type Constant
+const INCREMENT_COUNTER = 'INCREMENT_COUNTER'
+const DECREMENT_COUNTER = 'DECREMENT_COUNTER'
+
+// Action
+{
+  type: INCREMENT_COUNTER
+}
+
+{
+  type: DECREMENT_COUNTER
+}
