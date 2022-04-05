f8fc2d996c503944e0771d5cfcfc5364350c7058
Mrunal-dethekar <95629099+Mrunal-dethekar@users.noreply.github.com>
2022-04-06 00:12:12 +0530

    06thMar

--- Resume_Projects/admin-panel/src/App.js
+++ Resume_Projects/admin-panel/src/App.js
@@ -1,9 +1,31 @@
-
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import List from "./Pages/List/List";
+import Single from "./Pages/Single/Single";
+import New from "./Pages/New/New";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />}></Route>
+            <Route path="login" element={<Login />} />
+            <Route path="users">
+              <Route index element={<List />} />
+              <Route path=":userId" element={<Single />} />
+              <Route path="new" element={<New />} />
+            </Route>
+            <Route path="product">
+              <Route index element={<List />} />
+              <Route path=":productId" element={<Single />} />
+              <Route path="new" element={<New />} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
