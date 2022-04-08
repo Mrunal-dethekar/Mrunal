2a0506b1fd5a62a1763982a02ea668156edec8b7
Mrunal-dethekar <95629099+Mrunal-dethekar@users.noreply.github.com>
2022-04-08 14:34:40 +0530

    08April

--- Resume_Projects/admin-panel/src/Components/Widget/Widget.js
+++ Resume_Projects/admin-panel/src/Components/Widget/Widget.js
@@ -1,9 +1,25 @@
 import './widget.scss';
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import AccountBalanceWalletOutlinedIcon from "@mui/icons-material/AccountBalanceWalletOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlined";
 
-const Widget = () => {
+
+const Widget = ({ type }) => {
   return (
     <div className='widget'>
-     Widgets
+     <div className="left">
+       <span className="title">USER</span>
+       <span className="counter">12345</span>
+       <span className="link">See All User</span>
+     </div>
+     <div className="right">
+       <div className="percentage positive">
+         <KeyboardArrowUpIcon/>
+         20%</div>
+         <PersonOutlinedIcon className='icon'/>
+     </div>
     </div>
   )
 }
